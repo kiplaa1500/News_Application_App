216c996f9629b2f467caf4427daf27412fce9df1
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-06-05 11:39:30 +0300

    Adding request file to help in getting response

---
+++ app/request.py
@@ -0,0 +1,100 @@
+from .models import News
+import requests,json
+
+News = News
+
+
+# Getting api key
+api_key = None
+# Getting the news base url
+base_url = None
+base_url2 = None
+
+def configure_request(app):
+    global api_key,base_url,base_url2
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['NEWS_API_BASE_URL']
+    base_url2 = app.config['ARTICLE_API_BASE_URL']
+
+def get_news(category):
+    '''
+    Function that gets the json responce to our url request
+    '''
+    get_news_url = base_url.format(category,api_key)
+    print(get_news_url)
+    
+    get_news_response = requests.get(get_news_url).json()
+
+        
+
+    if get_news_response['articles']:
+        news_results_list = get_news_response['articles']
+        news_results = process_results(news_results_list)
+
+
+    
+
+    return news_results
+
+
+
+# def search_news(news_name):
+    
+#     search_news_url ='http://newsapi.org/v2/everything?q={}&from=2021-01-22&sortBy=publishedAt&apiKey={}'.format(news_name,api_key)
+    
+        
+#     get_news_response = requests.get(get_news_url).json()
+        
+
+#     search_news_results = None
+
+#     if search_news_response['results']:
+#         search_news_list = search_news_response['results']
+#         search_news_results = process_results(search_news_list)
+
+
+#     return search_news_results
+
+
+
+
+def process_results(news_list):
+    '''
+    Function  that processes the news result and transform them to a list of Objects
+    Args:
+        news_list: A list of dictionaries that contain news details
+    Returns :
+        news_results: A list of news objects
+    '''
+    news_results = []
+    for news_item in news_list:
+        title = news_item.get('title')
+        image = news_item.get('urlToImage')
+        description = news_item.get('description')
+        date = news_item.get('publishedAt')
+        article = news_item.get('url')
+
+        if image:
+            news_object = News(title,image,description,date,article)
+            news_results.append(news_object)
+
+    return news_results
+
+
+def get_article(source):
+    '''
+    Function that gets the json responce to our url request
+    '''
+    get_news_url = base_url.format(source,api_key)
+    
+    # get_news_response = json.loads(get_news_data)
+    get_news_response = requests.loads(get_news_data).json()
+
+        # news_results = None
+
+    if get_news_response['articles']:
+        news_results_list = get_news_response['articles']
+        news_results = process_results(news_results_list)
+   
+
+    return news_results
